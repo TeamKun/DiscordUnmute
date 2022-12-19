657bdc1ec8abe23aab445b42a574fffe2ea04c86
Kamesuta <kamesuta@gmail.com>
2022-12-20 03:05:30 +0900

    権限持ちがチャンネルに入ったときにサーバーミュートをつける

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 # discord.py
 import configparser
-import datetime
 import os
-
 import discord
 
 # 起動
@@ -12,19 +10,29 @@ print('起動しました')
 config = configparser.ConfigParser()
 config.read('config.ini', encoding='UTF-8')
 
+# チャンネル
 ss_channel = int(config['VC']['CHANNEL'])
+channel = None
+
+# 接続時ミュートモード
+join_mute_enabled = config['VC.JOIN_MUTE']['ENABLED'] == 'True'
+join_mute_ignore_users = [int(x) for x in config['VC.JOIN_MUTE']['IGNORED_USERS'].split(',')] 
 
 # 起動
 print(f"設定を読み込みました: channel={ss_channel}")
 
 
 # 接続に必要なオブジェクトを生成
-bot = discord.Client()
+intents = discord.Intents.default()
+intents.members = True
+intents.voice_states = True
+bot = discord.Client(intents=intents)
 
 
 # 起動時に動作する処理
 @bot.event
 async def on_ready():
+    global channel
     channel = bot.get_channel(ss_channel)
     if channel is None:
         print('チャンネルが見つかりません')
@@ -41,16 +49,27 @@ async def on_voice_state_update(member: discord.Member, before: discord.VoiceSta
     if before.channel == after.channel:
         return
 
-    # 指定チャンネルから抜けた時
-    if before.channel is None or before.channel.id != ss_channel:
-        return
+    # 指定チャンネルから他のチャンネルに移動したとき (VCから抜けたときはミュート解除できないので無視)
+    if before.channel is not None and before.channel.id == ss_channel and after.channel is not None:
+        # 関係ないチャンネル
+        if after.channel is not None and after.channel == after.channel.guild.afk_channel:
+            return
 
-    # 関係ないチャンネル
-    if after.channel is not None and after.channel == after.channel.guild.afk_channel:
-        return
+        # ミュート解除
+        await member.edit(mute=False)
 
-    # ミュート解除
-    await member.edit(mute=False)
+    # 指定チャンネルに入った時 && 発言権限を持っているとき
+    elif join_mute_enabled and after.channel is not None and after.channel.id == ss_channel:
+        # 除外ユーザーは無視
+        if member.id in join_mute_ignore_users:
+            return
+
+        # 発言権限を持っていない場合は無視
+        if not channel.permissions_for(member).speak:
+            return
+
+        # ミュート
+        await member.edit(mute=True)
 
 
 # 初期化
